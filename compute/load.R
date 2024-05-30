224a57b0449bddbf695d6dbe47cbb3c6d75eabf1
François-David Collin <Francois-David.Collin@umontpellier.fr>
2024-05-30 17:28:41 +0200

    update various

--- compute/load.R
+++
@@ -1,7 +0,0 @@
-
-# %%
-
-header <- sapply(read.table("compute/digits.csv", nrow=2), separator=",")
-# pandas_df <- read.csv("compute/digits.csv", skip = 2, header = FALSE, col.names = header)
-
-# head(pandas_df)
